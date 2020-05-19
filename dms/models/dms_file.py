4b6e1e3193484620d95da4ecccbb49fd39d378fa
Gelu <gelu.boros@rgbconsulting.com>
2020-05-19 18:39:02 +0200

    [FIX] Fix files naming

--- dms/models/dms_file.py
+++ dms/models/dms_file.py
@@ -459,7 +459,7 @@ class File(models.Model):
             return records
         directories = self._get_directories_from_database(records.ids)
         for directory in directories - directories._filter_access("read"):
-            records -= self.browse(directory.with_user(SUPERUSER_ID).mapped("files").ids)
+            records -= self.browse(directory.with_user(SUPERUSER_ID).mapped("file_ids").ids)
         return records
 
     def check_access(self, operation, raise_exception=False):
