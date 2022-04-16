3dd20a5322ce81d179b68d1b5109364933a74b70
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-04-16 17:31:03 -0300

    [FIX] dms_field: rename res_mimetype to mimetype https://github.com/OCA/dms/blob/46df04975e7df4976abf8003c25b8a0153674cd2/dms/migrations/14.0.1/pre-migration.py#L7

--- dms_field/static/src/js/base/dms_tree_controller.js
+++ dms_field/static/src/js/base/dms_tree_controller.js
@@ -448,7 +448,7 @@ odoo.define("dms.DmsTreeController", function (require) {
             return {
                 id: dt.id,
                 text: dt.data.display_name,
-                icon: mimetype.mimetype2fa(dt.data.res_mimetype, {prefix: "fa fa-"}),
+                icon: mimetype.mimetype2fa(dt.data.mimetype, {prefix: "fa fa-"}),
                 type: "file",
                 data: dt,
             };
@@ -456,7 +456,7 @@ odoo.define("dms.DmsTreeController", function (require) {
         _onDMSPreviewFile: function (ev) {
             var record = this._getDataPoint(ev.data.id, {raw: true});
             var fieldName = "content";
-            var file_mimetype = record.data.res_mimetype;
+            var file_mimetype = record.data.mimetype;
             var type = file_mimetype.split("/").shift();
             if (
                 type === "video" ||
@@ -467,9 +467,9 @@ odoo.define("dms.DmsTreeController", function (require) {
                     this,
                     [
                         {
-                            mimetype: record.data.res_mimetype,
+                            mimetype: record.data.mimetype,
                             id: record.data.id,
-                            fileType: record.data.res_mimetype,
+                            fileType: record.data.mimetype,
                             name: record.data.name,
                         },
                     ],
